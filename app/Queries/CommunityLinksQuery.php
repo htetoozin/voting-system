980b55f5840c2b9c8a9c08fbfa841411cfafb13b
htetoozin <htetoo.zin09@gmail.com>
2017-09-06 15:30:40 +0630

    Most Recent Or Most Popular

--- app/Queries/CommunityLinksQuery.php
+++ app/Queries/CommunityLinksQuery.php
@@ -8,14 +8,11 @@ use App\CommunityLink;
 class CommunityLinksQuery{
 	public function get($sortBypopular, $channel)
 	{
-		$orderBy = $sortBypopular ? 'vote_count' : 'updated_at';
-		return CommunityLink::with('votes', 'creator', 'channel')
+		$orderBy = $sortBypopular ? 'votes_count' : 'updated_at';
+		return CommunityLink::with('creator', 'channel')
+				->withCount('votes')
 				->forChannel($channel)
                 ->where('approved', 1)
-                ->leftJoin('community_links_votes', 'community_links_votes.community_link_id', '=', 'community_links.id')
-                ->selectRaw(
-                    'community_links.*, count(community_links_votes.id) as vote_count'
-                )
                 ->groupBy('community_links.id')
                 ->orderBy($orderBy, 'desc')
                 ->paginate(3);     	
--- resources/views/community/index.blade.php
+++ resources/views/community/index.blade.php
@@ -5,12 +5,21 @@
 	<div class="row">
 		<div class="col-md-8">
 			<h3>
-				<a href="/community">Community </a>
+				<a href="{{ request()->url() }}">Community </a>
 				@if($channel->exists)
 					<span> &mdash; {{ $channel->title }}</span>
 
 				@endif
 			</h3>
+
+			<ul class="nav nav-tabs">
+				<li class="{{ request()->exists('popular') ? '' : 'active' }}">
+					<a href="/community">Most Recent</a>
+				</li>
+				<li class="{{ request()->exists('popular') ? 'active' : ''}}">
+					<a href="?popular">Most Popular</a>
+				</li>
+			</ul>
 			
 			@include('community.list')
 		</div> <!-- Community Links -->
